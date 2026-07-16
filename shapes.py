9ac7880d17542aee883819d53e041726efd1d1ce
Amrutha GS <amruthags2006@gmail.com>
2026-07-16 16:15:58 +0530

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,23 @@
+from math import pi
+
+def area_circle(radius):
+    return pi * radius ** 2
+
+def area_square(s):
+    return s ** 2
+
+def main():
+    print("Area of circle:", area_circle(5))
+    print("Area of square:", area_square(4))
+
+from math import pi
+
+def area_circle(radius):
+    return pi * radius ** 2
+
+def area_square(s):
+    return s ** 2
+
+if __name__ == "__main__":
+    print("Area of circle:", area_circle(5))
+    print("Area of square:", area_square(4))
